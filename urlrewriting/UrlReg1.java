0a2db65db5607e4c573ec7c29049db5f9494c695
Tanay Saxena <tanay.saxena002@gmail.com>
2020-02-29 17:33:18 +0530

    url rewritting

---
+++ urlrewriting/UrlReg1.java
@@ -0,0 +1,32 @@
+package com.urlrewritting;
+
+import java.io.PrintWriter;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/url1")
+public class UrlReg1 extends HttpServlet
+{
+	public void doGet(HttpServletRequest req,HttpServletResponse res)
+	{
+		try
+		{
+			res.setContentType("text/html");
+      String uname=req.getParameter("uname");
+      String upwd=req.getParameter("upwd");
+      String umobno=req.getParameter("umobno");
+      System.out.println(uname+" "+upwd);
+     PrintWriter ps=res.getWriter();
+     ps.print("Servlet 1");
+     ps.println("<center><a href='url2?uname="+uname+"&upwd="+upwd+"&umobno="+umobno+"'>click here</a></center>");
+     
+     
+		}catch(Exception e)
+		{
+			System.out.println(e);
+		}
+	}
+}
---
+++ urlrewriting/urlRewriteReg.jsp
@@ -0,0 +1,21 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<title>Insert title here</title>
+</head>
+<body>
+<center>
+<table>
+<form action="url1" method="get">
+<tr><td>Username</td><td><input type="text" name="uname"></td></tr>
+<tr><td>Password</td><td><input type="text" name="upwd"></td></tr>
+<tr><td>Mobileno</td><td><input type="text" name="umobno"></td></tr>
+<tr><td><input type="submit" value="register">
+</form>
+</table>
+</center>
+</body>
+</html>
